bfda836ba279f47f610cdc395e66175fd5968936
s106062105 <s106062105@kitty.2y.idv.tw>
2019-10-07 17:16:38 +0800

    Update nomination

--- policy-iteration.py
+++ policy-iteration.py
@@ -2,33 +2,33 @@ import numpy as np
 import math
 import environment
 
-def initProb(map): # Initialize policy with all equal probability
-    prob = np.empty((map.shape[0], map.shape[1], 4))
-    #print(map)
-    for i in range(map.shape[0]):
-        for j in range(map.shape[1]):
-            if map[i][j] == 0:
+def initProb(reward): # Initialize policy with all equal probability
+    prob = np.empty((reward.shape[0], reward.shape[1], 4))
+    #print(reward)
+    for i in range(reward.shape[0]):
+        for j in range(reward.shape[1]):
+            if reward[i][j] == 0:
                 prob[i][j].fill(0.0)
             else:
                 prob[i][j].fill(1.0)
                 if i == 0:              # first row
                     prob[i][j][0] = 0.0
-                if i == map.shape[0]-1: # last row
+                if i == reward.shape[0]-1: # last row
                     prob[i][j][1] = 0.0
                 if j == 0:              # first col
                     prob[i][j][2] = 0.0
-                if j == map.shape[1]-1: # last col
+                if j == reward.shape[1]-1: # last col
                     prob[i][j][3] = 0.0
                 np.divide(prob[i][j], np.sum(prob[i][j]))
 
     return prob
 
-def evalPolicy(map, value, prob):
+def evalPolicy(reward, value, prob):
     new_value = np.copy(value)
     error = 0
-    for i in range(map.shape[0]):
-        for j in range(map.shape[1]):
-            new_value[i][j] = map[i][j]
+    for i in range(reward.shape[0]):
+        for j in range(reward.shape[1]):
+            new_value[i][j] = reward[i][j]
             if i > 0:
                 new_value[i][j] += prob[i][j][0] * value[i-1][j]
             if i < prob.shape[0]-1:
@@ -44,9 +44,9 @@ def evalPolicy(map, value, prob):
 
 def improvePolicy(value, prob):
     new_prob = np.copy(prob)
-    for i in range(map.shape[0]):
-        for j in range(map.shape[1]):
-            if map[i][j] != 0:
+    for i in range(reward.shape[0]):
+        for j in range(reward.shape[1]):
+            if reward[i][j] != 0:
                 next_state = np.empty(4)
                 next_state.fill(-1000)
                 if i > 0:
@@ -93,14 +93,14 @@ def printDir(prob):
 
 for i in range (4) :
     print("Testcase #" + str(i+1))
-    map = np.copy(environment.map[i])
-    print(map)
-    prob = initProb(map)
+    reward = np.copy(environment.map[i])
+    print(reward)
+    prob = initProb(reward)
 
     it = 1
-    value, error = evalPolicy(map, map, prob)
+    value, error = evalPolicy(reward, reward, prob)
     while (error > 0.000001) | (error < - 0.000001):
-        value, error = evalPolicy(map, value, prob)
+        value, error = evalPolicy(reward, value, prob)
         prob = improvePolicy(value, prob)
         it = it + 1
         #print("Iteration #" + str(it))
