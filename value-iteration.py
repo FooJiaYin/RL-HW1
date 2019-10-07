b1085b01fae02d0747903a09454b11b74a594b13
s106062105 <s106062105@kitty.2y.idv.tw>
2019-10-07 17:11:58 +0800

    Update nomination

--- value-iteration.py
+++ value-iteration.py
@@ -2,12 +2,12 @@ import numpy as np
 import math
 import environment
 
-def evalValue(map, value):
-    new_value = np.copy(map)
+def evalValue(reward, value):
+    new_value = np.copy(reward)
     error = 0
     for i in range(value.shape[0]):
         for j in range(value.shape[1]):
-            if map[i][j] != 0:
+            if reward[i][j] != 0:
                 next_state = np.empty(4)
                 next_state.fill(-1000)
                 if i > 0:
@@ -18,7 +18,7 @@ def evalValue(map, value):
                     next_state[2] = value[i][j-1]
                 if j < value.shape[1]-1:
                     next_state[3] = value[i][j+1]
-                new_value[i][j] = map[i][j] + max(next_state)
+                new_value[i][j] = reward[i][j] + max(next_state)
                 error += value[i][j] - new_value[i][j]
     return new_value, error
 
@@ -26,13 +26,13 @@ def evalValue(map, value):
 
 for i in range (4) :
     print("Testcase #" + str(i+1))
-    map = np.copy(environment.map[i])
-    print(map)
+    reward = np.copy(environment.map[i])
+    print(reward)
 
     it = 1
-    value, error = evalValue(map, map)
+    value, error = evalValue(reward, reward)
     while (error > 0.000001) | (error < - 0.000001):
-        value, error = evalValue(map, value)
+        value, error = evalValue(reward, value)
         it = it + 1
         #print("Iteration #" + str(it))
         #print(value)
